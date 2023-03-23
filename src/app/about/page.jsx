46924434242512616bb175346d86cd1ab86978a8
MZayed97 <mzayed97pro@gmail.com>
2023-03-23 21:24:52 +0200

    Create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6x1 mx-auto space-y-4">
+      <h1 className="text-2xl font-medium  mx-20">about</h1>
+    </div>
+  );
+}
